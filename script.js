9fe3da2e088eb0c6c5b59288c2ce8071af448645
manger32 <manger32@gmail.com>
2023-12-11 17:06:44 +0300

    added new html and binded

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//тут я игрался с alert'ами
+
 let username = prompt("Введите Ваше имя: ")
 let years = parseInt(prompt("Введите, сколько Вам лет: "))
 alert("Привет, " + username + ", тебе " + years + " лет.")
+
